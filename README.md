55adafddc529267239582a131a5dc8261d56398b
yui-ubc <yui.mikuriya@gmail.com>
2025-01-21 13:05:40 -0800

    changed two files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 2025-01-21-branch_conflict
+
+- `git switch -c <NAME>`: will create a new branch and switch to it
--- script.sh
+++ script.sh
@@ -1,3 +1,8 @@
+#this is a bash script file
+# run it like:
+# bash script.sh
+
 pwd
 ls
 echo "hello"
+echo "my second branch"
